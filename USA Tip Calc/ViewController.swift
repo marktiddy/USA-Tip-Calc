967c7dcab86991cdcfd63986e20a5764bc66fb5a
marktiddy <marktiddy@gmail.com>
2018-08-18 14:09:06 +0100

    Initial Commit

---
+++ USA Tip Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USA Tip Calc
+//
+//  Created by Developer on 18/08/2018.
+//  Copyright © 2018 Mark Tiddy Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
